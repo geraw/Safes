3b4aa40c305696a09e3d262ee71a083c2c854c62
Gera Weiss <gera.weiss@gmail.com>
2026-06-07 12:17:35 +0300

    Use Paiza checks through Apps Script

--- config.example.js
+++ config.example.js
@@ -1,4 +1,5 @@
 // Copy this file to config.js and paste the Google Apps Script Web App URL.
+// Google Apps Script stores the results in Google Sheets and checks solutions through Paiza.
 // You can also override this from the browser with ?api=<url>.
 window.CYBER_RIDDLES_API = "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec";
 window.CYBER_RIDDLES_API_MODE = "apps-script";
